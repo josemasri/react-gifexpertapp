5ac2374ecda03e5786b95e363f6507893d2b1e78
Jose Masri <josemasri2@gmail.com>
2020-07-10 19:19:05 -0500

    Docs added

---
+++ docs/precache-manifest.63d56e480e9469f46c6f7b9b8b5402f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987b7234cdb0660098261b9dce7e9ebe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec98e7cc35716f65bb08",
+    "url": "/static/css/main.9f1ce287.chunk.css"
+  },
+  {
+    "revision": "7e4f610a74e27287c487",
+    "url": "/static/js/2.230fb7e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.230fb7e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec98e7cc35716f65bb08",
+    "url": "/static/js/main.11874b76.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
